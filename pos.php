400bed1a2f191a33f9a955d996ea346c330e8429
amineaydi <amineaydi63@gmail.com>
2026-06-23 10:47:59 +0200

    Update pos.php

--- pos.php
+++ pos.php
@@ -4,7 +4,7 @@ session_start();
 require_once 'db.php';
 require_once 'theme.php';
 
-requireLogin();
+requirePosLogin();  // ✅ NEW - Only POS cashiers
 
 $bid = getBusinessId();
 $uid = getUserId();
